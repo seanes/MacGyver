ab425ae1fc8021120257491bf06c24b98a1a180c
Lilletass <terje.loenoey@norwegian.com>
2017-07-30 17:46:07 +0200

    Fixed mockdata import

--- src/mockdata/scoreboardMock.js
+++ src/mockdata/scoreboardMock.js
@@ -1,4 +1,4 @@
-export default [
+export const participants = [
   {
     "rank": "1",
     "name": "Terje Lønøy",
--- src/views/ScoreBoardView.js
+++ src/views/ScoreBoardView.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Helmet from 'react-helmet';
-import ScoreboardComponent from '../components/Scoreboard.js';
-import mockRows from '../mockdata/scoreboardMock.js';
+import ScoreboardComponent from '../components/Scoreboard';
+import { participants } from '../mockdata/scoreboardMock';
 
 export default () => {
   return (
     <div>
       <Helmet title="Scoreboard"/>
       <h1>Scoreboard</h1>
-      <ScoreboardComponent participants={mockRows}/>
+      <ScoreboardComponent participants={participants}/>
     </div>
   );
 }
