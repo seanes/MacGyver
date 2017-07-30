88c1910a3b944e2c975041402e43cb5b030f700e
Lilletass <terje.loenoey@norwegian.com>
2017-07-30 17:26:59 +0200

    Added scoreboard mock data

--- src/views/ScoreBoardView.js
+++ src/views/ScoreBoardView.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import Helmet from 'react-helmet';
 
+const mockRows = [
+  {
+    "rank": "1",
+    "name": "Terje Lønøy",
+    "score": "24"
+  },
+  {
+    "rank": "2",
+    "name": "Sean Erik Scully",
+    "score": "18"
+  },
+  {
+    "rank": "3",
+    "name": "Hulk Hogan",
+    "score": "9"
+  }
+];
+
+class ScoreboardComponent extends React.Component {
+  render() {
+    return (
+      <div>{this.props.participants.map((participant, i) => <ScoreboardRowComponent key={i} participant={participant} />) }</div>
+    );
+  }
+}
+
+class ScoreboardRowComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>{this.props.participant.rank}</div>
+        <div>{this.props.participant.name}</div>
+        <div>{this.props.participant.score}</div>
+      </div>
+    );
+  }
+}
+
 export default () => {
   return (
     <div>
       <Helmet title="Scoreboard"/>
       <h1>Scoreboard</h1>
+      <ScoreboardComponent participants={mockRows}/>
     </div>
   );
 }
