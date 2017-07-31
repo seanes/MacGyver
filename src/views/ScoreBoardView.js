edee0d98e8a7d3afabb91faa6ddf33473f394b85
Lilletass <terje.loenoey@norwegian.com>
2017-07-31 20:25:49 +0200

    Added scss

--- src/views/ScoreBoardView.js
+++ src/views/ScoreBoardView.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Helmet from 'react-helmet';
 import ScoreboardComponent from '../components/Scoreboard';
 import { participants } from '../mockdata/scoreboardMock';
+import styles from '../main.css';
 
 export default () => {
   return (
     <div>
       <Helmet title="Scoreboard"/>
       <h1>Scoreboard</h1>
-      <ScoreboardComponent participants={participants}/>
+      <ScoreboardComponent participants={participants} className={styles.scoreboard}/>
     </div>
   );
 }
