bd298e45c7a8a8baed6c0c36f5634f3236f7829b
Sean Scully <sean.erik.scully@gmail.com>
2017-08-01 20:58:29 +0200

    show active tab button

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,35 +1,55 @@
-import React from 'react'
-import {withRouter} from "react-router-dom"
-import styles from '../css/components/tabbar.css'
-
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import styles from '../css/components/tabbar.css';
 // Import icons
-import SVGProfile from '../icons/user.svg'
-import SVGParticipants from '../icons/team.svg'
-import SVGAgent from '../icons/spy.svg'
-import SVGTrophy from '../icons/trophy.svg'
+import SVGProfile from '../icons/user.svg';
+import SVGParticipants from '../icons/team.svg';
+import SVGAgent from '../icons/spy.svg';
+import SVGTrophy from '../icons/trophy.svg';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+const LINK_HOME = 'home';
+const LINK_PARTICIPANTS = 'participants';
+const LINK_ADD = 'add';
+const LINK_SCOREBOARD = 'score';
+const ICON_BASE_URL = '../icons/';
 
-const LINK_HOME = 'home'
-const LINK_PARTICIPANTS = 'participants'
-const LINK_ADD = 'add'
-const LINK_SCOREBOARD = 'score'
-
-const ICON_BASE_URL = '../icons/'
 
 class TabBarComponent extends React.Component {
-  handleClick (target) {
-    this.props.history.push(`/${target}`)
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: LINK_HOME
+    };
+  }
+
+  handleClick(target) {
+    this.props.history.push(`/${target}`);
+    this.setState({
+      activeTab: target
+    });
+  }
 
   render() {
+    const { activeTab } = this.state;
+
     return (
       <div className={styles.tabBar}>
-        <div onClick={() => this.handleClick(LINK_HOME, this)}><object type="image/svg+xml" data={SVGProfile} /> Profil</div>
-        <div onClick={() => this.handleClick(LINK_PARTICIPANTS, this)}><object type="image/svg+xml" data={SVGParticipants} />Deltakere</div>
-        <div onClick={() => this.handleClick(LINK_ADD, this)}><object type="image/svg+xml" data={SVGAgent} />Legg til agent</div>
-        <div onClick={() => this.handleClick(LINK_SCOREBOARD, this)}><object type="image/svg+xml" data={SVGTrophy} />Resultater</div>
+        <div className={cx({active: activeTab === LINK_HOME})} onClick={() => this.handleClick(LINK_HOME, this)}>
+          <object type="image/svg+xml" data={SVGProfile} /> Profil
+        </div>
+        <div className={cx({active: activeTab === LINK_PARTICIPANTS})} onClick={() => this.handleClick(LINK_PARTICIPANTS, this)}>
+          <object type="image/svg+xml" data={SVGParticipants} />Deltakere
+        </div>
+        <div className={cx({active: activeTab === LINK_ADD})} onClick={() => this.handleClick(LINK_ADD, this)}>
+          <object type="image/svg+xml" data={SVGAgent} />Legg til agent
+        </div>
+        <div className={cx({active: activeTab === LINK_SCOREBOARD})} onClick={() => this.handleClick(LINK_SCOREBOARD, this)}>
+          <object type="image/svg+xml" data={SVGTrophy} />Resultater
+        </div>
       </div>
     );
   }
-};
+}
 
 export default withRouter(TabBarComponent);
