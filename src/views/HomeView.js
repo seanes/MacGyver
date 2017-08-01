7ad324ea1f5446c1c0bffbbdb827a81e28208da3
Sean Scully <sean.erik.scully@gmail.com>
2017-08-02 00:58:58 +0200

    fixes log out button

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,29 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet'
 import CSSButton from '../css/base/buttons.css'
 import { withRouter } from 'react-router-dom';
 import { logout } from '../actions/sessionActions';
+import { connect } from 'react-redux';
 
-class LogoutButtonComponent extends React.Component {
+class HomeView extends React.Component {
+
   render () {
     const LogoutButton = withRouter(({ history }) => (
       <button
         className={CSSButton.success + ' ' + CSSButton.full}
-        onClick={() => logout(history)}>Logg ut</button>
+        onClick={() => this.props.dispatch(logout(history))}>Logg ut</button>
     ));
 
     return (
-      <LogoutButton />
+      <div>
+        <Helmet title="Profil"/>
+        <LogoutButton />
+      </div>
     )
   }
 }
 
-export default () => {
-  return (
-    <div>
-      <Helmet title="Profil"/>
-      <LogoutButtonComponent />
-    </div>
-  );
-}
+export default connect(null)(HomeView);
