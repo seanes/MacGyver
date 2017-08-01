03c1deb3bd9ba7f2fe24f7eec2ec0418f5d970be
Sean Scully <sean.erik.scully@gmail.com>
2017-08-01 22:52:39 +0200

    remove dev tool stuff

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
-import * as actionCreators from '../actions';
 import { sessionService } from 'redux-react-session';
 import thunkMiddleware from 'redux-thunk';
 
-
 export default function configureStore(preloadedState) {
 
-  const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__({ actionCreators });
-
-  if (!enhancer) {
-    console.warn('Install Redux DevTools Extension to inspect the app state: ' +
-      'https://github.com/zalmoxisus/redux-devtools-extension#installation')
-  }
-
-  const store = createStore(rootReducer, preloadedState, compose(applyMiddleware(thunkMiddleware), enhancer));
+  const store = createStore(rootReducer, preloadedState, compose(applyMiddleware(thunkMiddleware)));
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
