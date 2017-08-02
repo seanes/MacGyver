16d578c18b9c20c971a422ec832d2b6096d3cd15
Sean Scully <sean.erik.scully@gmail.com>
2017-08-02 17:49:06 +0200

    refactored svg icons to support fill

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import styles from '../css/components/tabbar.css';
-// Import icons
-import SVGProfile from '../icons/user.svg';
-import SVGParticipants from '../icons/team.svg';
-import SVGAgent from '../icons/spy.svg';
-import SVGTrophy from '../icons/trophy.svg';
 import classNames from 'classnames/bind';
+import ProfilIcon from './icons/ProfilIcon';
+import ParticipantsIcon from './icons/ParticipantsIcon';
+import AgentIcon from './icons/AgentIcon';
+import TrophyIcon from './icons/TrophyIcon';
+
 const cx = classNames.bind(styles);
 const LINK_HOME = '';
 const LINK_PARTICIPANTS = 'participants';
 const LINK_ADD = 'add';
 const LINK_SCOREBOARD = 'score';
-const ICON_BASE_URL = '../icons/';
-
 
 class TabBarComponent extends React.Component {
   constructor(props) {
@@ -36,16 +34,17 @@ class TabBarComponent extends React.Component {
     return (
       <div className={styles.tabBar}>
         <div className={cx({active: activeTab === LINK_HOME})} onClick={() => this.handleClick(LINK_HOME, this)}>
-          <object type="image/svg+xml" data={SVGProfile} /> Profil
+          <ProfilIcon isActive={activeTab === LINK_HOME}/>
+          Profil
         </div>
         <div className={cx({active: activeTab === LINK_PARTICIPANTS})} onClick={() => this.handleClick(LINK_PARTICIPANTS, this)}>
-          <object type="image/svg+xml" data={SVGParticipants} />Deltakere
+          <ParticipantsIcon isActive={activeTab === LINK_PARTICIPANTS}/>Deltakere
         </div>
         <div className={cx({active: activeTab === LINK_ADD})} onClick={() => this.handleClick(LINK_ADD, this)}>
-          <object type="image/svg+xml" data={SVGAgent} />Legg til agent
+          <AgentIcon isActive={activeTab === LINK_ADD}/>Legg til agent
         </div>
         <div className={cx({active: activeTab === LINK_SCOREBOARD})} onClick={() => this.handleClick(LINK_SCOREBOARD, this)}>
-          <object type="image/svg+xml" data={SVGTrophy} />Resultater
+          <TrophyIcon isActive={activeTab === LINK_SCOREBOARD}/>Resultater
         </div>
       </div>
     );
