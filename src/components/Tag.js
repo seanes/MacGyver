f533a0db429516ee06dc777ad51c7cbb726f1bd2
Lilletass <terje.loenoey@norwegian.com>
2017-08-02 21:15:56 +0200

    Made tags clickable

---
+++ src/components/Tag.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom';
+import styles from '../css/components/tag.css'
+
+class TagComponent extends React.Component {
+  handleClick() {
+    this.props.history.push(`/participants`);
+  }
+
+  render () {
+    return (
+      <div className={styles.tag} onClick={() => this.handleClick()}>{this.props.text}</div>
+    )
+  }
+}
+
+export default withRouter(TagComponent);
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import Tag from '../components/Tag'
 import styles from '../css/page/profile.css'
 import CSSButton from '../css/base/buttons.css'
 import { withRouter } from 'react-router-dom';
@@ -24,6 +25,20 @@ class HomeView extends React.Component {
             <div className={styles.image} >
               <img src={image} />
             </div>
+          </div>
+
+          <span className={styles.name}>Sean Erik Scrully</span>
+
+          <div className={styles.description}>Sean er en godgutt av natur, det er det som gjør han så godtroende. Who woulda thunk? Ikke jeg ihvertfall, kniz!</div>
+
+          <div className={styles.tagCloud}>
+            <Tag text="Godgutt" />
+            <Tag text="kramgut" />
+            <Tag text="knizegutt" />
+            <Tag text="morroklump" />
+            <Tag text="artigper" />
+            <Tag text="wannabeprogrammer" />
+            <Tag text="whiggah" />
           </div>
 
           <div className={styles.logout}>
