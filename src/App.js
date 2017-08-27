c64a3a1b07cc31dc0b9473a301ceceb88a8bb815
Sean Scully <sean.erik.scully@gmail.com>
2017-08-27 09:14:40 +0200

    trying to fix safari address bar issues, attempt 2

--- src/App.js
+++ src/App.js
@@ -11,45 +11,56 @@ import TabBar from './components/TabBar';
 import { connect } from 'react-redux';
 import styles from './css/main.css';
 
-const App = ({ authenticated, user, checked }) =>
-  <div>
-    <Router>
-      {checked &&
-        <div>
-          <div className={styles.page}>
-            <PrivateRoute
-              authenticated={authenticated}
-              exact
-              path="/"
-              component={HomeView}
-            />
-            <PrivateRoute
-              authenticated={authenticated}
-              path="/add"
-              component={AddView}
-            />
-            <PrivateRoute
-              authenticated={authenticated}
-              path="/participants"
-              component={PartcipantsView}
-            />
-            <PrivateRoute
-              authenticated={authenticated}
-              path="/score"
-              component={ScoreBoardView}
-            />
-            <Route path="/login" component={LoginView} />
-          </div>
-          {authenticated &&
-          <TabBar/>}
-        </div>}
-    </Router>
-  </div>;
+class App extends React.Component {
+
+  componentDidMount() {
+    window.scrollTo(0,1);
+  }
+
+  render() {
+    const { authenticated, checked } = this.props;
+    return (
+      <div>
+        <Router>
+          {checked &&
+          <div>
+            <div className={styles.page}>
+              <PrivateRoute
+                authenticated={authenticated}
+                exact
+                path="/"
+                component={HomeView}
+              />
+              <PrivateRoute
+                authenticated={authenticated}
+                path="/add"
+                component={AddView}
+              />
+              <PrivateRoute
+                authenticated={authenticated}
+                path="/participants"
+                component={PartcipantsView}
+              />
+              <PrivateRoute
+                authenticated={authenticated}
+                path="/score"
+                component={ScoreBoardView}
+              />
+              <Route path="/login" component={LoginView} />
+            </div>
+            {authenticated &&
+            <TabBar/>}
+          </div>}
+        </Router>
+      </div>
+    );
+  }
+}
+
 
 const mapStateToProps = ({ session, route }) => ({
   checked: session.checked,
   authenticated: session.authenticated,
-  user: session.user,
 });
 
 export default connect(mapStateToProps)(App);
